6ae7f12111904d21780824b607d1c2d13c7b5445
Arthur CHAZAL <achazal@e2r1p11.42.fr>
2016-05-17 20:33:12 +0200

    Done

--- Sully.c
+++ Sully.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <libgen.h>
 
 void loud(int i)
 {
@@ -36,36 +37,36 @@ void out(int fd, const char *s)
 
 const char laughing[] =     "#include <stdio.h>\n#include <fcntl.h>\n#include <"
     "stdlib.h>\n#include <unistd.h>\n#include <string.h"
-    ">\n\nvoid loud(int i)\n{\n	if (i == 0)\n		return ;\n\n	"
-    "char buffer[1024];\n	snprintf(buffer, sizeof(buff"
-    "er), \"gcc Sully_%%d.c -o Sully_%%d && ./Sully_%%"
-    "d\", i, i, i);\n	system(buffer);\n}\n\nvoid out(int f"
-    "d, const char *s)\n{\n	int i;\n\n	dprintf(fd, \"    \\"
-    "\"\");\n	for (i=0; s[i]; ++i) {\n		if (s[i] == '\\\\')"
-    "\n			dprintf(fd, \"\\\\\\\\\");\n		else if (s[i] == '\"')"
-    "\n			dprintf(fd, \"\\\\\\\"\");\n		else if (s[i] == '\\n'"
-    ")\n			dprintf(fd, \"\\\\n\");\n		else\n			dprintf(fd, \""
-    "%%c\", s[i]);\n		if (i %% 48 == 47)\n			dprintf(fd,"
-    " \"\\\"\\n    \\\"\");\n		}\n	dprintf(fd, \"\\\"\");\n}\n\nconst"
-    " char laughing[] = @@;\n\nint main(void)\n{\n	int i "
-    "= $$;\n	if (strcmp(__FILE__, \"Sully.c\"))\n		--i;\n	"
-    "int copy = i;\n	char buffer[42];\n	snprintf(buffer"
-    ", sizeof(buffer), \"Sully_%%d.c\", i);\n	printf(\"%%"
-    "s\\n\", buffer);\n	int fd = open(buffer, O_RDWR | O"
-    "_CREAT | O_SYNC);\n\n	for (i=0; laughing[i]; ++i) "
-    "{\n		if (laughing[i] == '@' && laughing[i+1] == '"
-    "@')\n		{\n			out(fd, laughing);\n			i++;\n		}\n		else"
-    " if (laughing[i] == '$' && laughing[i+1] == '$')"
-    "\n		{\n			dprintf(fd, \"%%d\", copy);\n			i++;\n		}\n		"
-    "else if (laughing[i] == '%%')\n		{\n			dprintf(fd,"
-    " \"%%%%\");\n			++i;\n		}\n		else\n			dprintf(fd, \"%%c"
-    "\", laughing[i]);\n	}\n	close(fd);\n	loud(copy);\n	re"
-    "turn 0;\n}\n";
+    ">\n#include <libgen.h>\n\nvoid loud(int i)\n{\n	if (i"
+    " == 0)\n		return ;\n\n	char buffer[1024];\n	snprintf"
+    "(buffer, sizeof(buffer), \"gcc Sully_%%d.c -o Sul"
+    "ly_%%d && ./Sully_%%d\", i, i, i);\n	system(buffer"
+    ");\n}\n\nvoid out(int fd, const char *s)\n{\n	int i;\n"
+    "\n	dprintf(fd, \"    \\\"\");\n	for (i=0; s[i]; ++i) {"
+    "\n		if (s[i] == '\\\\')\n			dprintf(fd, \"\\\\\\\\\");\n		e"
+    "lse if (s[i] == '\"')\n			dprintf(fd, \"\\\\\\\"\");\n		e"
+    "lse if (s[i] == '\\n')\n			dprintf(fd, \"\\\\n\");\n		e"
+    "lse\n			dprintf(fd, \"%%c\", s[i]);\n		if (i %% 48 ="
+    "= 47)\n			dprintf(fd, \"\\\"\\n    \\\"\");\n		}\n	dprintf"
+    "(fd, \"\\\"\");\n}\n\nconst char laughing[] = @@;\n\nint "
+    "main(void)\n{\n	int i = $$;\n	if (strcmp(basename(_"
+    "_FILE__), \"Sully.c\"))\n		--i;\n	int copy = i;\n	cha"
+    "r buffer[42];\n	snprintf(buffer, sizeof(buffer), "
+    "\"Sully_%%d.c\", i);\n	printf(\"%%s\\n\", buffer);\n	in"
+    "t fd = open(buffer, O_RDWR | O_CREAT | O_SYNC);\n"
+    "\n	for (i=0; laughing[i]; ++i) {\n		if (laughing[i"
+    "] == '@' && laughing[i+1] == '@')\n		{\n			out(fd,"
+    " laughing);\n			i++;\n		}\n		else if (laughing[i] ="
+    "= '$' && laughing[i+1] == '$')\n		{\n			dprintf(fd"
+    ", \"%%d\", copy);\n			i++;\n		}\n		else if (laughing["
+    "i] == '%%')\n		{\n			dprintf(fd, \"%%%%\");\n			++i;\n"
+    "		}\n		else\n			dprintf(fd, \"%%c\", laughing[i]);\n	"
+    "}\n	close(fd);\n	loud(copy);\n	return 0;\n}\n";
 
 int main(void)
 {
 	int i = 5;
-	if (strcmp(__FILE__, "Sully.c"))
+	if (strcmp(basename(__FILE__), "Sully.c"))
 		--i;
 	int copy = i;
 	char buffer[42];
