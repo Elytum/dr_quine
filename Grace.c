a2212ba63f7379b0e66669f23809a0178c94e47d
Arthur CHAZAL <achazal@e1r3p13.42.fr>
2017-11-19 19:52:55 +0100

    No explicit function in Grace

--- Grace.c
+++ Grace.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <fcntl.h>
 #define GO int main(void) {go();}
-#define BIDULE0 "lol"
-#define BIDULE1 "lol"
+#define BIDULE0 int go(void)
+#define BIDULE1 void gadgeto(int fd, const char *s)
 /*
     This program will print its own source when run.
 */
 
-void gadgeto(int fd, const char *s)
+BIDULE1
 {
 	int i;
 
@@ -29,25 +29,25 @@ void gadgeto(int fd, const char *s)
 
 const char progdata[] =
     "#include <stdio.h>\n#include <fcntl.h>\n#define GO"
-    " int main(void) {go();}\n#define BIDULE0 \"lol\"\n#d"
-    "efine BIDULE1 \"lol\"\n/*\n    This program will pri"
-    "nt its own source when run.\n*/\n\nvoid gadgeto(int"
-    " fd, const char *s)\n{\n	int i;\n\n	dprintf(fd, \"   "
-    " \\\"\");\n	for (i=0; s[i]; ++i) {\n		if (s[i] == '\\\\"
-    "')\n			dprintf(fd, \"\\\\\\\\\");\n		else if (s[i] == '\""
-    "')\n			dprintf(fd, \"\\\\\\\"\");\n		else if (s[i] == '\\"
-    "n')\n			dprintf(fd, \"\\\\n\");\n		else\n			dprintf(fd,"
-    " \"%c\", s[i]);\n		if (i % 48 == 47)\n			dprintf(fd,"
-    " \"\\\"\\n    \\\"\");\n		}\n	dprintf(fd, \"\\\"\");\n}\n\nconst"
-    " char progdata[] =\n@@;\n\nint go(void)\n{\n	int i;\n	"
-    "int fd = open(\"Grace_kid.c\", O_RDWR | O_CREAT | "
-    "O_SYNC);\n\n	for (i=0; progdata[i]; ++i) {\n		if (p"
-    "rogdata[i] == '@' && progdata[i+1] == '@')\n		{\n	"
-    "		gadgeto(fd, progdata);\n			i++;\n		} else\n		dpri"
-    "ntf(fd, \"%c\", progdata[i]);\n	}\n	return 0;\n}\n\nGO\n"
-    "";
+    " int main(void) {go();}\n#define BIDULE0 int go(v"
+    "oid)\n#define BIDULE1 void gadgeto(int fd, const "
+    "char *s)\n/*\n    This program will print its own "
+    "source when run.\n*/\n\nBIDULE1\n{\n	int i;\n\n	dprintf"
+    "(fd, \"    \\\"\");\n	for (i=0; s[i]; ++i) {\n		if (s["
+    "i] == '\\\\')\n			dprintf(fd, \"\\\\\\\\\");\n		else if (s"
+    "[i] == '\"')\n			dprintf(fd, \"\\\\\\\"\");\n		else if (s"
+    "[i] == '\\n')\n			dprintf(fd, \"\\\\n\");\n		else\n			dp"
+    "rintf(fd, \"%c\", s[i]);\n		if (i % 48 == 47)\n			dp"
+    "rintf(fd, \"\\\"\\n    \\\"\");\n		}\n	dprintf(fd, \"\\\"\");"
+    "\n}\n\nconst char progdata[] =\n@@;\n\nBIDULE0\n{\n	int "
+    "i;\n	int fd = open(\"Grace_kid.c\", O_RDWR | O_CREA"
+    "T | O_SYNC);\n\n	for (i=0; progdata[i]; ++i) {\n		i"
+    "f (progdata[i] == '@' && progdata[i+1] == '@')\n	"
+    "	{\n			gadgeto(fd, progdata);\n			i++;\n		} else\n		"
+    "dprintf(fd, \"%c\", progdata[i]);\n	}\n	return 0;\n}\n"
+    "\nGO\n";
 
-int go(void)
+BIDULE0
 {
 	int i;
 	int fd = open("Grace_kid.c", O_RDWR | O_CREAT | O_SYNC);
